c23b0e6af6226492281e0709c07bb00709d0e29e
gituser0192 <hs2162943@gmail.com>
2025-05-30 19:22:47 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,15 @@
-output "vpc_id" {
-  value       = aws_vpc.main.id
-  description = "The ID of the VPC"
-}
+# output "vpc_id" {
+#   value = aws_vpc.main.id
+# }
 
-output "public_subnets" {
-  value       = aws_subnet.public[*].id
-  description = "IDs of the public subnets"
-}
+# output "public_subnets" {
+#   value = aws_subnet.public[*].id
+# }
 
-output "ecs_cluster_name" {
-  value       = aws_ecs_cluster.medusa_cluster.name
-  description = "Name of the ECS cluster"
-}
+# output "ecs_cluster_name" {
+#   value = aws_ecs_cluster.medusa_cluster.name
+# }
 
-output "load_balancer_dns" {
-  value       = aws_lb.medusa_lb.dns_name
-  description = "DNS of the application load balancer"
-}
+# output "load_balancer_dns" {
+#   value = aws_lb.medusa_lb.dns_name
+# }
